3ca5cb7f0e182f85f700f16ca067a8e2bb5eb2da
MusaDAC <54865936+musadac@users.noreply.github.com>
2023-08-28 12:41:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,22 +3,10 @@ const nextConfig = {
 output: 'export',
 }
 
-module.exports = nextConfig
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
   output: 'export',
 }
