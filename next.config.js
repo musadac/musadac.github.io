a7e1bd5594eac62925ba64fdb9ba61ee2b160c26
Musa Cheema <musa.cheema@teradata.com>
2025-03-16 12:32:03 +0500

    update 4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/github-pages" : "", // Set basePath only in production
-  assetPrefix: process.env.NODE_ENV === "production" ? "/github-pages/" : "", // Ensure assets load correctly
-  trailingSlash: true, // Needed for GitHub Pages
+  basePath: process.env.NODE_ENV === "production" ? "/github-pages" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/github-pages/" : "",
+  trailingSlash: true,
   output: "export",
+  distDir: "out" // Ensures Next.js exports files to "out/"
 };
 
 module.exports = nextConfig;
