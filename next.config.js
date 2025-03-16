e4c4ffa5f5778d31357a4f1ece3c33e1e8ffc211
MusaDAC <54865936+musadac@users.noreply.github.com>
2025-03-16 12:40:33 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/github-pages" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/github-pages/" : "",
-  trailingSlash: true,
-  output: "export",
-  distDir: "out" // Ensures Next.js exports files to "out/"
-};
+output: 'export',
+}
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
+  output: 'export',
+}
